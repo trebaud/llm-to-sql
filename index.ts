8e8fe849dbc6ffb3022b16d2d1b95b18515a9650
thomasr <rebaud.thomas@gmail.com>
2024-07-28 15:41:42 -0400

    use bun sql lite driver with data.db pocketbase file

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import ollama from "ollama";
+import { Database } from "bun:sqlite";
 
 import dbSchema from "./pb/pb_schema.json";
 
@@ -22,19 +23,20 @@ export async function getSQLQueryFromLLM(userQuery: string) {
     The output should look like: "SELECT address, phone_number FROM businesses FROM wifi = true"
     For example if the query is: "How many of the businesses offer wifi at their location?"
     The output should look like: "SELECT COUNT(*) FROM businesses WHERE wifi = true"
-    Only output the SQL statement and NOTHING ELSE!
+    Only output the sql statement and NOTHING else!
   `;
   const dbSchema = getPrettyDBSchemaForLLM();
   const query = `${systemPrompt}. Here is the db schema: ${dbSchema}. Here is the user query: ${userQuery}`;
-  console.log({ query });
   const resp = await ollama.generate({
     model: LLM,
     prompt: query,
   });
 
-  console.log(resp.response);
-  // TODO check if output is valid SQL query
   return resp.response;
 }
 
-getSQLQueryFromLLM(process.env.QUERY as string);
+const userQuery = await getSQLQueryFromLLM(process.env.QUERY as string);
+
+const db = new Database("./pb/pb_data/data.db");
+const dbQuery = db.query(userQuery);
+console.log(dbQuery.get());
